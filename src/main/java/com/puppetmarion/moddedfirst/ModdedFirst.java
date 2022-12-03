99370c028b32e3d668af4aebda47575df03d0974
PuppetMarion <noamannaseer58@gmail.com>
2022-12-03 13:40:05 +0530

    Added a comment

--- src/main/java/com/puppetmarion/moddedfirst/ModdedFirst.java
+++ src/main/java/com/puppetmarion/moddedfirst/ModdedFirst.java
@@ -1,10 +1,12 @@
 package com.puppetmarion.moddedfirst;
 
 import com.mojang.logging.LogUtils;
+import com.puppetmarion.moddedfirst.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -27,8 +29,10 @@ public class ModdedFirst
     // Added a comment
     public ModdedFirst()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
